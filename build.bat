2e7ab04f5d7f363202a2c46cb38d8824a2617630
Syed <NSyed2@its.jnj.com>
2022-05-12 12:32:13 +0000

    Build release

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
+rm -rf build dist tray.spec                  
 pyinstaller.exe --onefile --add-data "*.exe;." --noconsole --windowed --add-data "*.pdf;." --add-data "*.png;." --add-data "*.ini;." tray.py
